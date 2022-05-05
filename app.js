2bbfc17c373743a6c4dc24349bb51f5066d858e7
Igor Burenkov <49118930+EveryBarry@users.noreply.github.com>
2022-05-05 15:23:10 +0300

    feat: add ESlint + gitignore

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 
+
 class Keyboard {
     constructor(lang) {
         this.lang = lang;
     }
 
+    /**
+     * main method init keyboard
+     */
     init() {
         this.favicon();
         console.log('KeyBoard init');
         console.log('Lang = ' + this.lang)
     }
 
+    //change favicon (RU / EN)
     favicon() {
         let head = document.querySelector('head');
-        this.lang == 'RU' || 'EN'
+        // eslint-disable-next-line no-constant-condition
+        this.lang === 'RU' || 'EN'
             ? head.innerHTML += `<link rel="shortcut icon" href="favicons/favicon-${this.lang}-32x32.png" type="image/x-icon">`
             : console.log('ERROR: lang not RU / EN');
+    }
+
+    generateKeyboard() {
+
     }
 
     generateKeys() {
